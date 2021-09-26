8e76de69a7942df55e771099adcaabd0c3ed52b2
swpudp <swpudp@163.com>
2021-09-26 10:51:21 +0800

    array

--- src/OfdSharp/Primitives/Array.cs
+++ src/OfdSharp/Primitives/Array.cs
@@ -14,83 +14,87 @@ namespace OfdSharp.Primitives
         /// <summary>
         /// 元素收容
         /// </summary>
-        private readonly List<string> _array = new List<string>();
+        private readonly List<string> _elements;
 
         /// <summary>
-        /// 获取一个单位矩阵变换参数
+        /// 获取一个单位矩阵变换参数 0 0 1
         /// </summary>
         /// <returns>单位CTM举证</returns>
-        public static Array UnitCTM()
+        public static Array UnitMatrix => new Array("1", "0", "0", "1", "0", "0");
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="arr"></param>
+        public Array(params string[] arr)
         {
-            return new Array(
-                "1", "0", // 0
-                "0", "1", // 0
-                "0", "0"  // 1
-            );
+            _elements = new List<string>(arr.Length);
+            _elements.AddRange(arr);
         }
 
-        public Array MtxMul(Array array)
+        /// <summary>
+        /// 矩阵相乘
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public Array Muliply(Array array)
         {
-            if (this._array.Count != 6 || array._array.Count != 6)
+            if (_elements.Count != 6 || array.Size != 6)
             {
                 throw new ArgumentOutOfRangeException();
             }
-            double[,] a = ToMtx();
-            double[,] b = array.ToMtx();
-
-            double[,] res = new double[3, 3];
-
+            double[,] a = ToMatrix();
+            double[,] b = array.ToMatrix();
+            double[,] result = new double[3, 3];
             for (int k = 0; k < 3; k++)
             {
                 for (int i = 0; i < 3; i++)
                 {
                     for (int j = 0; j < 3; j++)
                     {
-                        res[i, j] += a[i, k] * b[k, j];
+                        result[i, j] += a[i, k] * b[k, j];
                     }
                 }
             }
-
-            return new Array(Format(res[0, 0]), Format(res[0, 1]), Format(res[1, 0]), Format(res[1, 1]), Format(res[2, 0]), Format(res[2, 1]));
+            return new Array(Format(result[0, 0]), Format(result[0, 1]), Format(result[1, 0]), Format(result[1, 1]), Format(result[2, 0]), Format(result[2, 1]));
         }
 
-
-        public Array(params string[] arr)
-        {
-            _array = new List<string>(arr.Length);
-            foreach (string s in arr)
-            {
-                _array.Add(s);
-            }
-        }
-
+        /// <summary>
+        /// 格式化
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns></returns>
         public static string Format(double d)
         {
             return d.ToString(CultureInfo.InvariantCulture);
         }
 
-        public int Size => _array.Count;
+        /// <summary>
+        /// 元素个数
+        /// </summary>
+        public int Size => _elements.Count;
 
-        public double[,] ToMtx()
+        /// <summary>
+        /// 转换成矩阵
+        /// </summary>
+        /// <returns></returns>
+        public double[,] ToMatrix()
         {
             if (Size != 6)
             {
                 throw new ArgumentOutOfRangeException("矩阵数组必须有 9个元素");
             }
-            double[,] mtx = new double[3, 3];
-            mtx[0, 0] = double.Parse(_array.ElementAt(0));
-            mtx[0, 1] = double.Parse(_array.ElementAt(1));
-            mtx[0, 2] = 0;
-
-            mtx[1, 0] = double.Parse(_array.ElementAt(2));
-            mtx[1, 1] = double.Parse(_array.ElementAt(3));
-            mtx[1, 2] = 0;
-
-            mtx[2, 0] = double.Parse(_array.ElementAt(4));
-            mtx[2, 1] = double.Parse(_array.ElementAt(5));
-            mtx[2, 2] = 1;
-
-            return mtx;
+            double[,] matrix = new double[3, 3];
+            matrix[0, 0] = double.Parse(_elements.ElementAt(0));
+            matrix[0, 1] = double.Parse(_elements.ElementAt(1));
+            matrix[0, 2] = 0;
+            matrix[1, 0] = double.Parse(_elements.ElementAt(2));
+            matrix[1, 1] = double.Parse(_elements.ElementAt(3));
+            matrix[1, 2] = 0;
+            matrix[2, 0] = double.Parse(_elements.ElementAt(4));
+            matrix[2, 1] = double.Parse(_elements.ElementAt(5));
+            matrix[2, 2] = 1;
+            return matrix;
         }
     }
 }
