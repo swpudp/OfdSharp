9a3892045624deca8cc1011b597df5b5208aa147
swpudp <swpudp@163.com>
2022-03-11 18:32:26 +0800

    ofd.xml写入

--- src/OfdSharp/Writer/OfdWriter.cs
+++ src/OfdSharp/Writer/OfdWriter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OfdSharp.Primitives.Ofd;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -9,39 +10,49 @@ namespace OfdSharp.Writer
 {
     public class OfdWriter
     {
-        private MemoryStream _root;
+        private XElement _root;
 
 
         public void WriteOfdRoot()
         {
-            XNamespace xNamespace = XNamespace.Get("http://www.ofdspec.org/2016");
-            //XmlNamespaceManager oManager = new XmlNamespaceManager(new NameTable());
-            //oManager.AddNamespace("ofd", xNamespace.NamespaceName);
-            XDeclaration declaration = new XDeclaration("1.0", "UTF-8", string.Empty);
-            //XDocument document = new XDocument(declaration);
-
-
-            XElement root = new XElement(XName.Get("ofd", xNamespace.NamespaceName));
-            root.SetAttributeValue("DocType", "OFD");
-            //root.SetAttributeValue(XName.Get("ofd", XNamespace.Xmlns.NamespaceName), "ofd");
-
-            XNamespace xmlns = "XSDName";
-            XNamespace xsi = @"http://www.w3.org/2001/XMLSchema-instance";
-            XNamespace schemaloc = @"XSDName XSDName.xsd";
-            XDocument document = new XDocument(
-                new XElement(xmlns + "BaseReport",
-                new XAttribute(xsi + "schemaLocation", schemaloc),
-                new XAttribute(XNamespace.Xmlns + "ns1", xmlns),
-                new XAttribute(XNamespace.Xmlns + "xsi", xsi)));
-
-            document.Add(root);
-            _root = new MemoryStream();
-            document.Save(_root);
+            XNamespace aw = "http://www.ofdspec.org/2016";
+            XElement docInfo = new XElement(aw + "DocInfo");
+            docInfo.Add(new XElement(aw + "DocID", "c9a4ced42f284320964bf1e630d1fa2a"));
+            docInfo.Add(new XElement(aw + "Author", "China Tax"));
+            docInfo.Add(new XElement(aw + "CreationDate", "2020-11-16"));
+            List<CustomData> customDatas = new List<CustomData>
+            {
+                new CustomData { Name = "template-version", Value = "1.0.20.0422" },
+                new CustomData { Name = "native-producer", Value = "SuwellFormSDK" },
+                new CustomData { Name = "producer-version", Value = "1.0.20.0603" },
+                new CustomData { Name = "发票代码", Value = "032001900311" },
+                new CustomData { Name = "发票号码", Value = "25577301" },
+                new CustomData { Name = "合计税额", Value = "***" },
+                new CustomData { Name = "合计金额", Value = "181.14" },
+                new CustomData { Name = "开票日期", Value = "2020年11月12日" },
+                new CustomData { Name = "校验码", Value = "58569 30272 33709 75117" },
+                new CustomData { Name = "购买方纳税人识别号", Value = "91510700205412308D" },
+                new CustomData { Name = "销售方纳税人识别号", Value = "91320111339366503A" }
+            };
+            XElement customDataElement = new XElement(aw + "CustomDatas");
+            foreach (var item in customDatas)
+            {
+                customDataElement.Add(new XElement(aw + "CustomData", new XAttribute("Name", item.Name), item.Value));
+            }
+            docInfo.Add(customDataElement);
+            XElement docBody = new XElement(aw + "DocBody", docInfo);
+            docBody.Add(new XElement(aw + "DocRoot", "Doc_0/Document.xml"));
+            docBody.Add(new XElement(aw + "Signatures", "Doc_0/Signs/Signatures.xml"));
+            _root = new XElement(aw + "OFD", new XAttribute(XNamespace.Xmlns + "ofd", "http://www.ofdspec.org/2016"), new XAttribute("DocType", "OFD"), new XAttribute("Version", "1.1"), docBody);
         }
 
         public byte[] Flush()
         {
-            return _root.ToArray();
+            using (var stream = new MemoryStream())
+            {
+                _root.Save(stream);
+                return stream.ToArray();
+            }
         }
     }
 }
